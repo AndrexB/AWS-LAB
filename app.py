07f00e3cb586cceca7f507a039c3d44796c77445
AndrexB <162334594+AndrexB@users.noreply.github.com>
2026-01-29 16:43:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/env")
-def env_info():
-    # mostra un dettaglio utile sul deploy e la porta in uso
-    port = os.environ.get("PORT", "undefined")
-    return f"Hello from AWS App Runner! PORT={port}", 200
+def home():
+    return "Hello from AWS App Runner 🚀"
 
 @app.route("/health")
 def health():
-    # endpoint semplice per health checks
     return "OK", 200
 
 if __name__ == "__main__":
-    # per debug locale
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
