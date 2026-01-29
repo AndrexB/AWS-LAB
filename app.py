f7ca54493d1e31f4b9f7884fea7ff0016686c943
AndrexB <162334594+AndrexB@users.noreply.github.com>
2026-01-29 17:44:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello App Runner 🚀"
+    name = os.environ.get("NAME", "World")
+    return f"Hello, {name}! 🚀"
 
 if __name__ == "__main__":
-    # Port 8000 perché App Runner espone quella porta
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
